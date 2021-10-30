5784b0c23e28770447ede64e2de7bf6c893befdf
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-30 18:38:49 +0300

    added class news

--- models.py
+++ models.py
@@ -9,3 +9,19 @@ class articles:
         self.publishedAt = publishedAt
         self.author = author
         self.url = url
+      
+
+class News:
+    '''
+    all news class to define news objects
+    '''
+    def __init__(self, name, author, url,description, country,category, id):
+        self.name = name
+        self.author = author
+        self.url = url
+        self.description = description
+        self.country = country
+        self.category = category
+        self.id = id
+    
+
