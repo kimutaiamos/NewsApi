660eabec9c1023c6f5449f581e8faab37215d802
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-30 18:46:16 +0300

    added changes to get api key

--- request.py
+++ request.py
@@ -7,3 +7,29 @@ from .models import Articles, News
 api_key = None
 base_url = None
 articles_url = None
+
+
+def configure_request(app):
+    global api_key, base_url, articles_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['ALL_NEWS_API_URL']
+    articles_url = app.config ['ARTICLES_BASE_URL']
+
+
+def get_news(category):
+    '''function that gets the json response to the url request
+    '''
+    get_news_url = base_url.format(category, api_key)
+    print(get_news_url)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
+            news_results = process_results(news_results_list)
+
+    return news_results
