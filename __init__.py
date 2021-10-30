073aa2b800271ec4b68ec408a963b5e22feefe8d
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-10-30 18:37:22 +0300

    added class articles

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, app
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+
+Bootstrap = Bootstrap()
+def create_app(config_name):
+    app = Flask(__name__)
+
+    #creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    #initializing flask
+    Bootstrap.init_app(app)
+    #registering blueprint
+    from .main  import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+     #setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
+ 
---
+++ models.py
@@ -0,0 +1,11 @@
+class articles:
+    """
+    articles class to define articles object
+    """
+    def __init__(self, title, description, image, publishedAt, author, url):
+        self.title = title
+        self.description = description
+        self.image = image
+        self.publishedAt = publishedAt
+        self.author = author
+        self.url = url
